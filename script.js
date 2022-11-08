d0cec69d996a6994bf16de9b63ecfb09d60a4b0f
VitaliyFinchuk <117160118+dujsvy@users.noreply.github.com>
2022-11-08 12:39:01 +0200

    почала виконувати завдання

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+var input1 = document.getElementById("task1"),
+    res = document.getElementById("result"),
+    button = document.getElementById("button"),
+    counter = 0 ;
+if ( ){
 
+}
+else {
+
+} 
